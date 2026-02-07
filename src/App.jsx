d4d6f81c8248c5016f68d9f710cccc2b57d79733
Mintbes <65181304+Mintbes@users.noreply.github.com>
2026-02-07 21:14:14 +0100

    Add project structure and core files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import WhyDelegate from './components/WhyDelegate';
+import StakingInfo from './components/StakingInfo';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="font-sans antialiased text-gray-900 bg-white min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Hero />
+        <About />
+        <WhyDelegate />
+        <StakingInfo />
+        <Gallery />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
